ea5baae4d38af8962a77ab2198aaa5d528db5fc6
Antonios Papadopoulos <antonios.papadopan@gmail.com>
2022-11-21 18:44:19 +0100

    add form for login

--- components/Welcome/Login/index.tsx
+++ components/Welcome/Login/index.tsx
@@ -1,11 +1,40 @@
-import {  Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { Box, Button, Flex, FormControl, Input, Stack, WarningOutlineIcon } from 'native-base'
 
 export const Login = () => {
+
+  const [email, setEmail] = useState("")
+  const [pwd, setPwd] = useState("")
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <Flex flex={1} justifyContent="space-between" padding={5}>
+      <FormControl isRequired>
+        <Stack space={5}>
+          <Stack>
+            <FormControl.Label>Email</FormControl.Label>
+            <Input size={"xl"} variant="underlined" type="text" p={2}
+              value={email}
+              onChangeText={setEmail}
+            placeholder="john@doe.com" />
+          </Stack>
+          <Stack>
+            <FormControl.Label>Password</FormControl.Label>
+            <Input size={"xl"} variant="underlined" type='password'
+              value={pwd}
+              onChangeText={setPwd}
+            p={2} placeholder="Password"/>
+            <FormControl.HelperText>
+              Must be atleast 6 characters.
+            </FormControl.HelperText>
+            <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+              Atleast 6 characters are required.
+            </FormControl.ErrorMessage>
+          </Stack>
+        </Stack>
+      </FormControl>
+      <Box>
+        <Button>Log In</Button>
+      </Box>
+    </Flex>
   )
 }
 
