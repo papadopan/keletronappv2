01fd2cf5a6d0c8397edd176c8f4bed4be5677a86
Antonios Papadopoulos <antonios.papadopan@gmail.com>
2022-11-21 18:57:15 +0100

    add logo welcome image

--- components/Welcome/Login/index.tsx
+++ components/Welcome/Login/index.tsx
@@ -7,30 +7,36 @@ export const Login = () => {
   const [pwd, setPwd] = useState("")
   return (
     <Flex flex={1} justifyContent="space-between" padding={5}>
-      <FormControl isRequired>
-        <Stack space={5}>
-          <Stack>
-            <FormControl.Label>Email</FormControl.Label>
-            <Input size={"xl"} variant="underlined" type="text" p={2}
-              value={email}
-              onChangeText={setEmail}
-            placeholder="john@doe.com" />
+      <Stack>
+        <FormControl isRequired mb={10}>
+          <Stack space={5}>
+            <Stack>
+              <FormControl.Label>Email</FormControl.Label>
+              <Input size={"xl"} variant="underlined" type="text" p={2}
+                value={email}
+                onChangeText={setEmail}
+              placeholder="john@doe.com" />
+            </Stack>
+            <Stack>
+              <FormControl.Label>Password</FormControl.Label>
+              <Input size={"xl"} variant="underlined" type='password'
+                value={pwd}
+                onChangeText={setPwd}
+              p={2} placeholder="Password"/>
+              <FormControl.HelperText>
+                Must be atleast 6 characters.
+              </FormControl.HelperText>
+              <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+                Atleast 6 characters are required.
+              </FormControl.ErrorMessage>
+            </Stack>
           </Stack>
-          <Stack>
-            <FormControl.Label>Password</FormControl.Label>
-            <Input size={"xl"} variant="underlined" type='password'
-              value={pwd}
-              onChangeText={setPwd}
-            p={2} placeholder="Password"/>
-            <FormControl.HelperText>
-              Must be atleast 6 characters.
-            </FormControl.HelperText>
-            <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-              Atleast 6 characters are required.
-            </FormControl.ErrorMessage>
-          </Stack>
+        </FormControl>
+        <Stack space={2}>
+          <Button variant="link">Forgot Password?</Button>
+          <Button variant="link">Resend Activation email</Button>
         </Stack>
-      </FormControl>
+      </Stack>
       <Box>
         <Button>Log In</Button>
       </Box>
--- components/Welcome/index.tsx
+++ components/Welcome/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Button, Center, Flex, Text, VStack, Image } from 'native-base'
 
-export const Welcome = ({ navigation  }) => {
+export const Welcome = ({ navigation }) => {
   return (
     <Flex flex={1} justifyContent="space-between" padding={10}>
         <Center>
-          <Text>Keletron Tennis Academy</Text>
           <Image
-          source={{uri: "https://wallpaperaccess.com/full/317501.jpg"}}
+          source={{uri: "https://images.unsplash.com/photo-1587280501635-68a0e82cd5ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"}}
           alt="Alternate Text"
-          size="xl" />
+          size="2xl"
+          borderRadius={10}
+          mb="10"
+          />
+          <Text fontSize={"xl"}>Keletron Tennis Academy</Text>
         </Center>
         <VStack space={5} mt={10}>
           <Button size={"lg"} onPress={()=>navigation.navigate("SignUp")}>Sign Up</Button>
