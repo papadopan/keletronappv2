ffe591fd3f1331becf08b031f779b4c8ca7a3c01
Antonios Papadopoulos <antonios.papadopan@gmail.com>
2022-11-24 22:15:52 +0100

    add icons to the Profile screen

--- components/Home/Profile/index.tsx
+++ components/Home/Profile/index.tsx
@@ -1,37 +1,59 @@
 import React from 'react'
-import { Box , Center, Flex, Stack, Text, Icon, Button} from 'native-base'
+import { Box, Center, Flex, Stack, Text, Button } from 'native-base'
+import Icon from 'react-native-vector-icons/AntDesign'
 
 export const Profile = () => {
   return (
     <Flex padding={5} paddingTop={15} justifyContent="space-between" flex={1}>
       <Center>
-        <Box  backgroundColor="yellow.100"  borderRadius={50} padding="5" mb={8}>
-          <Text fontSize={"4xl"} fontWeight="bold">AP</Text>
+        <Box backgroundColor="yellow.100" borderRadius={50} padding="5" mb={8}>
+          <Text fontSize={'4xl'} fontWeight="bold">
+            AP
+          </Text>
         </Box>
-          <Text fontSize={"2xl"} fontWeight="semibold">Antonios</Text>
-          <Text fontSize={"2xl"} fontWeight="semibold">Papadopoulos</Text>
-          <Text fontSize={"md"} fontWeight="hairline">antonios.papadopan@gmail.com</Text>
+        <Text fontSize={'2xl'} fontWeight="semibold">
+          Antonios
+        </Text>
+        <Text fontSize={'2xl'} fontWeight="semibold">
+          Papadopoulos
+        </Text>
+        <Text fontSize={'md'} fontWeight="hairline">
+          antonios.papadopan@gmail.com
+        </Text>
       </Center>
       <Stack space={4}>
-        <Flex flexDirection="row" justifyContent="space-between" backgroundColor="cyan.50" padding="5" borderRadius="6">
+        <Flex
+          flexDirection="row"
+          justifyContent="space-between"
+          backgroundColor="cyan.50"
+          padding="5"
+          borderRadius="6"
+        >
           <Text>Language</Text>
-          <Flex>
-            Greek arrow
+          <Flex flexDirection={'row'}>
+            <Text mr="3">Greek</Text>
+            <Icon name="right" size={20} />
           </Flex>
         </Flex>
-        <Flex flexDirection="row" justifyContent="space-between" backgroundColor="cyan.50" padding="5" borderRadius="6">
+        <Flex
+          flexDirection="row"
+          justifyContent="space-between"
+          backgroundColor="cyan.50"
+          padding="5"
+          borderRadius="6"
+        >
           <Text>Theme</Text>
-          <Flex>
-            Dark arrow
+          <Flex flexDirection={'row'}>
+            <Text mr="3">Dark</Text>
+            <Icon name="right" size={20} />
           </Flex>
         </Flex>
         <Flex>
-            <Button variant="outline">Sign out</Button>
+          <Button variant="outline" leftIcon={<Icon name="logout" size={20} />}>
+            Sign out
+          </Button>
         </Flex>
       </Stack>
     </Flex>
   )
 }
-
-
-
