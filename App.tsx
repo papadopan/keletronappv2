d68d74014893d493ad29bfd5c389df788cd69550
Antonios Papadopoulos <antonios.papadopan@gmail.com>
2022-12-03 20:08:35 +0100

    save the userId to the async storage

--- App.tsx
+++ App.tsx
@@ -8,23 +8,40 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaView, StatusBar, useColorScheme } from 'react-native';
 
 import { Auth } from './components/Auth';
 import { WelcomeNavigator } from './components/Welcome/WelcomeNAvigator';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { AppNavigator } from 'types/navigation';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createNativeStackNavigator<AppNavigator>();
 
-const App = () => {
+const App = ({ navigation }) => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? 'blue' : 'white',
     flex: 1,
   };
+
+  const checkIfUserExists = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@userId');
+      // it means that the user is already logged inf
+      if (value) {
+        navigation.navigate('Auth');
+      }
+    } catch (e) {
+      // error reading value
+    }
+  };
+
+  useEffect(() => {
+    checkIfUserExists();
+  }, []);
 
   return (
     <SafeAreaView style={backgroundStyle}>
--- hooks/useLogIn.ts
+++ hooks/useLogIn.ts
@@ -1,6 +1,7 @@
 import { gql } from '@apollo/client';
 import { useMutation } from '@tanstack/react-query';
 import request from 'graphql-request';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type Credentials = {
   password: string;
@@ -19,11 +20,23 @@ const mutation = gql`
   }
 `;
 
+const storeData = async (value: string) => {
+  try {
+    await AsyncStorage.setItem('@userId', value);
+  } catch (e) {
+    // saving error
+  }
+};
+
 export const useLogIn = () => {
   return useMutation({
     mutationFn: (values: Credentials) =>
       request('http://localhost:4000/graphql', mutation, {
         credentials: values,
       }),
+    onSuccess: data => {
+      const { login } = data;
+      storeData(login.id);
+    },
   });
 };
