7b192102399742ea83b804a9ab64a4f07077c2af
Antonios Papadopoulos <antonios.papadopan@gmail.com>
2022-12-06 22:40:32 +0100

    re order Login screen

--- components/Welcome/Login/index.tsx
+++ components/Welcome/Login/index.tsx
@@ -23,95 +23,90 @@ export const Login = ({ navigation }) => {
   }, [isSuccess]);
 
   return (
-    <Flex flex={1} justifyContent="space-between" padding={5}>
-      <Stack>
-        <Formik
-          initialValues={{
-            email: '',
-            password: '',
-          }}
-          onSubmit={v => mutate(v)}
-          validationSchema={LoginSchema}
-        >
-          {({ handleChange, handleSubmit, values, errors, touched }) => (
-            <Stack space={5} justifyContent="space-between">
-              <Box>
-                <FormControl
-                  isRequired
-                  mb={4}
-                  isInvalid={
-                    errors.email && values.email.length > 0 ? true : undefined
-                  }
+    <Formik
+      initialValues={{
+        email: '',
+        password: '',
+      }}
+      onSubmit={v => mutate(v)}
+      validationSchema={LoginSchema}
+    >
+      {({ handleChange, handleSubmit, values, errors, touched }) => (
+        <Flex justifyContent="space-between" p={5} flex={1}>
+          <Box>
+            <Box>
+              <FormControl
+                isRequired
+                mb={4}
+                isInvalid={
+                  errors.email && values.email.length > 0 ? true : undefined
+                }
+              >
+                <FormControl.Label>Email</FormControl.Label>
+                <Input
+                  size={'xl'}
+                  variant="underlined"
+                  type="text"
+                  p={2}
+                  value={values.email}
+                  onChangeText={handleChange('email')}
+                  placeholder="john@doe.com"
+                  keyboardType="email-address"
+                />
+                <FormControl.ErrorMessage
+                  leftIcon={<WarningOutlineIcon size="xs" />}
                 >
-                  <FormControl.Label>Email</FormControl.Label>
-                  <Input
-                    size={'xl'}
-                    variant="underlined"
-                    type="text"
-                    p={2}
-                    value={values.email}
-                    onChangeText={handleChange('email')}
-                    placeholder="john@doe.com"
-                    keyboardType="email-address"
-                  />
-                  <FormControl.ErrorMessage
-                    leftIcon={<WarningOutlineIcon size="xs" />}
-                  >
-                    {errors.email}
-                  </FormControl.ErrorMessage>
-                </FormControl>
-                <FormControl
-                  isRequired
-                  isInvalid={
-                    errors.password && values.password.length > 0
-                      ? true
-                      : undefined
-                  }
+                  {errors.email}
+                </FormControl.ErrorMessage>
+              </FormControl>
+              <FormControl
+                isRequired
+                isInvalid={
+                  errors.password && values.password.length > 0
+                    ? true
+                    : undefined
+                }
+              >
+                <FormControl.Label>Password</FormControl.Label>
+                <Input
+                  size={'xl'}
+                  variant="underlined"
+                  type="password"
+                  value={values.password}
+                  onChangeText={handleChange('password')}
+                  p={2}
+                  placeholder="Password"
+                />
+                <FormControl.HelperText>
+                  Must be atleast 6 characters.
+                </FormControl.HelperText>
+                <FormControl.ErrorMessage
+                  leftIcon={<WarningOutlineIcon size="xs" />}
                 >
-                  <FormControl.Label>Password</FormControl.Label>
-                  <Input
-                    size={'xl'}
-                    variant="underlined"
-                    type="password"
-                    value={values.password}
-                    onChangeText={handleChange('password')}
-                    p={2}
-                    placeholder="Password"
-                  />
-                  <FormControl.HelperText>
-                    Must be atleast 6 characters.
-                  </FormControl.HelperText>
-                  <FormControl.ErrorMessage
-                    leftIcon={<WarningOutlineIcon size="xs" />}
-                  >
-                    {errors.password}
-                  </FormControl.ErrorMessage>
-                </FormControl>
-              </Box>
-              <Button onPress={() => handleSubmit()} isLoading={false}>
-                Log In
+                  {errors.password}
+                </FormControl.ErrorMessage>
+              </FormControl>
+            </Box>
+            <Stack space={2} mt={8}>
+              <Button
+                variant="link"
+                onPress={() => navigation.navigate('ForgotPassword')}
+              >
+                Forgot Password?
+              </Button>
+              <Button
+                variant="link"
+                onPress={() => navigation.navigate('ActivationEmail')}
+              >
+                Resend Activation email
               </Button>
             </Stack>
-          )}
-        </Formik>
-        <Stack space={2}>
-          <Button
-            variant="link"
-            onPress={() => navigation.navigate('ForgotPassword')}
-          >
-            Forgot Password?
+          </Box>
+          <Button onPress={() => handleSubmit()} isLoading={false}>
+            Log In
           </Button>
-          <Button
-            variant="link"
-            onPress={() => navigation.navigate('ActivationEmail')}
-          >
-            Resend Activation email
-          </Button>
-        </Stack>
-      </Stack>
-      <Box>
-        <Button>Log In</Button>
-      </Box>
-    </Flex>
+        </Flex>
+      )}
+    </Formik>
   );
 };
