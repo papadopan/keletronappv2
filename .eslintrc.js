8b20706d1e0e3e73dd5966d1bfa8f5fd4a033649
Antonios Papadopoulos <antonios.papadopan@gmail.com>
2022-12-31 00:40:51 +0100

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,14 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:react/recommended', 'standard-with-typescript'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ['react'],
   rules: {
+    semi: [2, 'always']
   }
-}
+};
