9589f2402adc647f30fdb2266da569816aaee79d
Antonios Papadopoulos <antonios.papadopan@gmail.com>
2023-04-27 18:57:37 +0200

    ios file updates

--- ios/KeletronAppv2/AppDelegate.mm
+++ ios/KeletronAppv2/AppDelegate.mm
@@ -1,10 +1,12 @@
 #import "AppDelegate.h"
 
+
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
 #import <React/RCTAppSetupUtils.h>
+#import <Firebase.h>
 
 #if RCT_NEW_ARCH_ENABLED
 #import <React/CoreModulesPlugins.h>
@@ -15,6 +17,7 @@
 #import <ReactCommon/RCTTurboModuleManager.h>
 
 #import <react/config/ReactNativeConfig.h>
+
 
 static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 
@@ -31,6 +34,8 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
+
   RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
---
+++ ios/build/KeletronAppv2.build/Debug-iphoneos/KeletronAppv2.build/DerivedSources/KeletronAppv2_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char KeletronAppv2VersionString[];
+ extern const double KeletronAppv2VersionNumber;
+
+ const unsigned char KeletronAppv2VersionString[] __attribute__ ((used)) = "@(#)PROGRAM:KeletronAppv2  PROJECT:KeletronAppv2-1" "\n";
+ const double KeletronAppv2VersionNumber __attribute__ ((used)) = (double)1.;
